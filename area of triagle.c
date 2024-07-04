8a1056035e7881101ccf87c1e347398fb951735a
Suba Nahian <155134439+subanahian@users.noreply.github.com>
2024-07-04 21:09:47 +0600

    Add files via upload

---
+++ area of triagle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float base,height,area;
+    printf("Enter the number of ;base,height:\n");
+    scanf("%f%f",&base,&height);
+    area=0.5*base*height;
+    printf("area=%f",area);
+    return 0;
+}
