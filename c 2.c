1c124890f38d574c7b7557ce0c67356b34c67d6c
Suba Nahian <155134439+subanahian@users.noreply.github.com>
2024-07-05 22:57:06 +0600

    Add files via upload

---
+++ c 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double R,A;
+
+    printf("enter float number\n");
+    A = 3.14159 * R * R;
+    printf("The area of the circle with a radius of %.2lf is %.2lf\n", R, A);
+
+    return 0;
+}
