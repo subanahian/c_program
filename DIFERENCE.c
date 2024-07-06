9042057d4f36e1c7a184f0ab61c7e36194787076
Suba Nahian <155134439+subanahian@users.noreply.github.com>
2024-07-06 11:17:11 +0600

    Add files via upload

---
+++ DIFERENCE.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int A,B,C,D,product;
+printf("Enter Integer Value :\n");
+scanf("%d%d%d%d",&A,&B,&C,&D);
+product=(A*B - C*D);
+printf(" DIFFERENCE = %d",product);
+return 0;
+}
