1242b90ab059f8463c6c4d6c0e702982636fb608
Suba Nahian <155134439+subanahian@users.noreply.github.com>
2024-07-07 22:56:32 +0600

    Add files via upload

---
+++ even btwfive.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n,x=0;
+    for (i=0;i<5;i++) {
+        scanf("%d",&n);
+        if (number%2==0) {
+            x++;}
+            }
+    printf("%d valores pares\n",x);
+    return 0;
+}
+
